9416b5cb9afd7b66b235e6c2718cf2a84bcfcea5
Shogo Iwano <shiwano@gmail.com>
2015-04-29 19:10:16 +0900

    wip

--- Assets/Scripts/Submarine/SubmarineView.cs
+++ Assets/Scripts/Submarine/SubmarineView.cs
@@ -3,7 +3,32 @@ using System.Collections;
 
 namespace Submarine
 {
-    public class SubmarineView : MonoBehaviour
+    public class SubmarineView : Photon.MonoBehaviour
     {
+        private Vector3 receivedPosition = Vector3.zero;
+        private Quaternion receivedRotation = Quaternion.identity;
+
+        private void Update()
+        {
+            if (!photonView.isMine)
+            {
+                transform.position = Vector3.Lerp(transform.position, receivedPosition, Time.deltaTime * 5);
+                transform.rotation = Quaternion.Lerp(transform.rotation, receivedRotation, Time.deltaTime * 5);
+            }
+        }
+     
+        private void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+        {
+            if (stream.isWriting)
+            {
+                stream.SendNext(transform.position);
+                stream.SendNext(transform.rotation);
+            }
+            else
+            {
+                this.receivedPosition = (Vector3)stream.ReceiveNext();
+                this.receivedRotation = (Quaternion)stream.ReceiveNext();
+            }
+        }
     }
 }
