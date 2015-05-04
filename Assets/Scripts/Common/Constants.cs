24476c4fbaf7da9c6cb6cd80ed89731a86db2513
Shogo Iwano <shiwano@gmail.com>
2015-05-04 16:39:20 +0900

    wip

--- Assets/Scripts/Common/Constants.cs
+++ Assets/Scripts/Common/Constants.cs
@@ -8,5 +8,6 @@ namespace Submarine
         public const string Version = "0.1";
 
         public const string SubmarinePrefab = "Submarines/Submarine";
+        public const string TorpedoPrefab = "Torpedos/Torpedo";
     }
 }
