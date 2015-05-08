9db323dfbbe0c8d5f904dfd041aca03773982f9d
Shogo Iwano <shiwano@gmail.com>
2015-05-08 12:50:58 +0900

    wip

---
+++ Assets/Scripts/Radar/Radar.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using Zenject;
+
+namespace Radar
+{
+    public class Radar : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject pinger;
+        [SerializeField]
+        GameObject playerPinPrefab;
+        [SerializeField]
+        GameObject enemyPinPrefab;
+        [SerializeField]
+        GameObject torpedoPinPrefab;
+        [SerializeField]
+        GameObject lookoutPinPrefab;
+        [SerializeField]
+        GameObject pinContainer;
+    }
+}
