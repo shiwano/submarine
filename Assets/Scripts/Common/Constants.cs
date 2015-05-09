9ece7b3cf68e915fb363bdda594800626791c643
Shogo Iwano <shiwano@gmail.com>
2015-05-09 17:36:34 +0900

    wip

--- Assets/Scripts/Common/Constants.cs
+++ Assets/Scripts/Common/Constants.cs
@@ -9,9 +9,9 @@ namespace Submarine
         public const int FrameRate = 30;
         public const int MapLength = 400;
 
-        public const string SubmarinePrefab = "Submarines/Submarine";
-        public const string TorpedoPrefab = "Torpedos/Torpedo";
-        public const string ExplosionEffectPrefab = "Effects/Explosion";
+        public const string SubmarinePrefab = "Battle/Submarines/Submarine";
+        public const string TorpedoPrefab = "Battle/Torpedos/Torpedo";
+        public const string ExplosionEffectPrefab = "Battle/Effects/Explosion";
 
         public static float Fps { get { return 1f / Time.deltaTime; } }
         public static float FpsRate { get { return Fps / FrameRate; } }
