0b4bb3f8ae0d32c7f8a208899f4cc70232f20b88
Shogo Iwano <shiwano@gmail.com>
2015-07-11 20:59:44 +0900

    server: Add DatabaseRewinder settings

--- server/spec/rails_helper.rb
+++ server/spec/rails_helper.rb
@@ -29,6 +29,16 @@ Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include Requests::JsonHelpers, type: :request
+
+  config.before :suite do
+    DatabaseRewinder.clean_all
+  end
+
+  config.after :each do
+    DatabaseRewinder.clean
+  end
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
--- server/spec/support/request_helpers.rb
+++ server/spec/support/request_helpers.rb
@@ -5,7 +5,3 @@ module Requests
     end
   end
 end
-
-RSpec.configure do |config|
-  config.include Requests::JsonHelpers, type: :request
-end
