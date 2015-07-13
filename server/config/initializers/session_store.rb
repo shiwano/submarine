f743e1084ed6ceda3b32e71d1de9a8e9858d3608
Shogo Iwano <shiwano@gmail.com>
2015-07-14 08:37:20 +0900

    server: Add redis settings

--- server/config/initializers/session_store.rb
+++ server/config/initializers/session_store.rb
@@ -7,7 +7,9 @@ Rails.application.config.session_store :redis_session_store, {
   serializer: :json,
   on_redis_down: ->(e, env, sid) { raise ApplicationError::SessionServerDown.new(e) },
   redis: {
-    db: 3,
+    host: Settings.redis[:host],
+    port: Settings.redis[:port],
+    db: Settings.redis[:db],
     expire_after: 120.minutes,
     key_prefix: 'submarine:session:'
   }
--- server/spec/support/database_cleaner.rb
+++ server/spec/support/database_cleaner.rb
@@ -2,7 +2,7 @@ require 'database_cleaner'
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner[:redis].db = 'redis://localhost:6379/3'
+    DatabaseCleaner[:redis].db = "redis://#{Settings.redis[:host]}:#{Settings.redis[:port]}/#{Settings.redis[:db]}"
     DatabaseCleaner[:active_record].strategy = :transaction
     DatabaseCleaner.clean_with :truncation
   end
