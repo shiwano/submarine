881bd9e1a7dc344166b3c6267cb4ad65a5f8eca2
Shogo Iwano <shiwano@gmail.com>
2015-07-13 22:10:34 +0900

    server: rake db:migrate

--- server/app/models/user.rb
+++ server/app/models/user.rb
@@ -1,3 +1,19 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :integer          not null, primary key
+#  name             :string(255)      not null
+#  crypted_password :string(255)
+#  salt             :string(255)
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+# Indexes
+#
+#  index_users_on_name  (name) UNIQUE
+#
+
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 end
--- server/db/schema.rb
+++ server/db/schema.rb
@@ -11,6 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150711144907) do
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name",             limit: 255, null: false
+    t.string   "crypted_password", limit: 255
+    t.string   "salt",             limit: 255
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["name"], name: "index_users_on_name", unique: true, using: :btree
 
 end
