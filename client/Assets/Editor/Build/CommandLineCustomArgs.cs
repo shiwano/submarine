aec021ebbeaec3fd141b5a6e5812b2abf4b0d955
Shogo Iwano <shiwano@gmail.com>
2015-08-30 11:55:46 +0900

    client: Add CommandLineCustomArgs

---
+++ client/Assets/Editor/Build/CommandLineCustomArgs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class CommandLineCustomArgs
+{
+    readonly Dictionary<string, string> customArgs = new Dictionary<string, string>();
+
+    public string[] CommandLineArgs
+    {
+        get { return Environment.GetCommandLineArgs(); }
+    }
+
+    public CommandLineCustomArgs()
+    {
+        foreach (var arg in CommandLineArgs)
+        {
+            var keyAndValue = arg.Split('=');
+
+            if (keyAndValue.Length == 2)
+            {
+                customArgs.Add(keyAndValue[0], keyAndValue[1]);
+            }
+        }
+    }
+
+    public string GetArg(string argumentName)
+    {
+        return customArgs[argumentName];
+    }
+
+    public string GetArg(string argumentName, string defaultValue)
+    {
+        string value;
+        return customArgs.TryGetValue(argumentName, out value) ? value : defaultValue;
+    }
+}
