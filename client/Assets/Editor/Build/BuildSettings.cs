9e8e78c0744b16b6095479ad1750a2fe59272853
Shogo Iwano <shiwano@gmail.com>
2015-08-30 21:23:20 +0900

    client: Add BuildSettings

---
+++ client/Assets/Editor/Build/BuildSettings.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Linq;
+
+public class BuildSettings
+{
+    public BuildTarget BuildTarget;
+    public string BundleIdentifier;
+    public string BundleVersion;
+    public string ProductName;
+
+    public string[] SceneNames
+    {
+        get { return EditorBuildSettings.scenes.Select(x => x.path).ToArray(); }
+    }
+
+    public string OutputPath
+    {
+        get
+        {
+            return BuildTarget == BuildTarget.iOS ?
+                System.IO.Directory.GetCurrentDirectory() + "/iOSXCodeProject" :
+                System.IO.Directory.GetCurrentDirectory() + "/build.apk";
+        }
+    }
+
+    public BuildSettings(CommandLineCustomArgs args)
+    {
+        BundleIdentifier = args.GetArg("bundleIdentifier");
+        BundleVersion = args.GetArg("bundleVersion");
+        ProductName = args.GetArg("productName");
+
+        var target = args.GetArg("buildTarget");
+
+        switch (target)
+        {
+            case "ios":
+                BuildTarget = BuildTarget.iOS;
+                break;
+            case "android":
+                BuildTarget = BuildTarget.Android;
+                break;
+            default:
+                Debug.LogError("Not supported build target: " + target);
+                return;
+        }
+    }
+}
