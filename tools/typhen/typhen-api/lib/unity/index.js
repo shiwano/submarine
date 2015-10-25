fe70ddd16658f7049728940d5eca03358e56ed91
Shogo Iwano <shiwano@gmail.com>
2015-10-26 02:00:50 +0900

    tools: Add support for RealTimeMessage to typhen-api unity

--- tools/typhen/typhen-api/lib/unity/index.js
+++ tools/typhen/typhen-api/lib/unity/index.js
@@ -50,20 +50,21 @@ module.exports = function(typhen, options, helpers) {
       return name;
     },
 
-    generate: function(generator, types, modules) {
-      glob.sync(path.join(generator.outputDirectory, 'TyphenApi/Generated/**/*.cs')).forEach(function(path) {
+    generate: function(g, types, modules) {
+      glob.sync(path.join(g.outputDirectory, 'TyphenApi/Generated/**/*.cs')).forEach(function(path) {
         fs.removeSync(path);
       });
 
-      generator.generateFiles('lib/unity/templates/Core', '**/*.cs', 'TyphenApi/Generated/Core');
-      generator.generate('lib/unity/templates/Type/Void.cs', 'TyphenApi/Generated/Type/Void.cs');
+      g.generateFiles('lib/unity/templates/Core', '**/*.cs', 'TyphenApi/Generated/Core');
+      g.generate('lib/unity/templates/Type/Void.cs', 'TyphenApi/Generated/Type/Void.cs');
+      g.generate('lib/unity/templates/Type/RealTimeMessage.cs', 'TyphenApi/Generated/Type/RealTimeMessage.cs');
 
       modules.forEach(function(module) {
         if (module.functions.length > 0) {
-          generator.generate('lib/unity/templates/WebApi/WebApi.hbs', 'upperCamelCase:TyphenApi/Generated/WebApi/**/*.cs', module);
+          g.generate('lib/unity/templates/WebApi/WebApi.hbs', 'upperCamelCase:TyphenApi/Generated/WebApi/**/*.cs', module);
 
           if (!module.parentModule) {
-            generator.generateUnlessExist('lib/unity/templates/Controller/WebApiController.hbs', 'upperCamelCase:TyphenApi/Controller/WebApi/*.cs', module);
+            g.generateUnlessExist('lib/unity/templates/Controller/WebApiController.hbs', 'upperCamelCase:TyphenApi/Controller/WebApi/*.cs', module);
           }
         }
       });
@@ -71,31 +72,31 @@ module.exports = function(typhen, options, helpers) {
       types.forEach(function(type) {
         switch (type.kind) {
           case typhen.SymbolKind.Enum:
-            generator.generate('lib/unity/templates/Type/Enum.hbs', 'upperCamelCase:TyphenApi/Generated/Type/**/*.cs', type);
+            g.generate('lib/unity/templates/Type/Enum.hbs', 'upperCamelCase:TyphenApi/Generated/Type/**/*.cs', type);
             break;
           case typhen.SymbolKind.Interface:
             if (!type.isGenericType || type.typeArguments.length === 0) {
-              generator.generate('lib/unity/templates/Type/Class.hbs', 'upperCamelCase:TyphenApi/Generated/Type/**/*.cs', type);
+              g.generate('lib/unity/templates/Type/Class.hbs', 'upperCamelCase:TyphenApi/Generated/Type/**/*.cs', type);
             }
             break;
           case typhen.SymbolKind.ObjectType:
-            generator.generate('lib/unity/templates/Type/Class.hbs', 'upperCamelCase:TyphenApi/Generated/Type/**/*.cs', type);
+            g.generate('lib/unity/templates/Type/Class.hbs', 'upperCamelCase:TyphenApi/Generated/Type/**/*.cs', type);
             break;
         }
       });
 
       if (!options.excludeUnityFiles) {
-        generator.generate('lib/unity/templates/Unity/WebApiRequestSenderWWW.cs', 'TyphenApi/Generated/Core/Unity/WebApiRequestSenderWWW.cs');
-        generator.generate('lib/unity/templates/Unity/WebApiRequest.Unity.cs', 'TyphenApi/Generated/Core/Unity/WebApiRequest.Unity.cs');
-        generator.generate('lib/unity/templates/Unity/FormDataSerializer.cs', 'TyphenApi/Generated/Core/Unity/FormDataSerializer.cs');
+        g.generate('lib/unity/templates/Unity/WebApiRequestSenderWWW.cs', 'TyphenApi/Generated/Core/Unity/WebApiRequestSenderWWW.cs');
+        g.generate('lib/unity/templates/Unity/WebApiRequest.Unity.cs', 'TyphenApi/Generated/Core/Unity/WebApiRequest.Unity.cs');
+        g.generate('lib/unity/templates/Unity/FormDataSerializer.cs', 'TyphenApi/Generated/Core/Unity/FormDataSerializer.cs');
 
         if (options.includeUniRxFiles) {
-          generator.generate('lib/unity/templates/Unity/WebApiRequest.UniRx.cs', 'TyphenApi/Generated/Core/Unity/WebApiRequest.UniRx.cs');
+          g.generate('lib/unity/templates/Unity/WebApiRequest.UniRx.cs', 'TyphenApi/Generated/Core/Unity/WebApiRequest.UniRx.cs');
         }
       }
 
       if (!options.excludeMiniJSON) {
-        generator.generateUnlessExist('lib/unity/templates/Import/MiniJSON.cs', 'TyphenApi/Import/MiniJSON.cs');
+        g.generateUnlessExist('lib/unity/templates/Import/MiniJSON.cs', 'TyphenApi/Import/MiniJSON.cs');
       }
     }
   };
