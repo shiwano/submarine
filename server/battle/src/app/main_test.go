f47c5acea310654f2f6cb80248764f6a0ebf651a
Shogo Iwano <shiwano@gmail.com>
2015-12-26 13:46:34 +0900

    battle: Rewrite main_test by using goconvey

--- server/battle/src/app/main_test.go
+++ server/battle/src/app/main_test.go
@@ -4,16 +4,50 @@ import (
 	"app"
 	"app/typhenapi/core"
 	"app/typhenapi/type/submarine/battle"
-	battleapi "app/typhenapi/websocket/submarine/battle"
+	api "app/typhenapi/websocket/submarine"
 	"github.com/Sirupsen/logrus"
 	"github.com/gin-gonic/gin"
 	"github.com/gorilla/websocket"
-	"github.com/stretchr/testify/assert"
+	. "github.com/smartystreets/goconvey/convey"
 	"io"
 	"net/http/httptest"
 	"strings"
 	"testing"
 )
+
+type clientSession struct {
+	*websocket.Conn
+	api *api.WebSocketAPI
+}
+
+func (session *clientSession) Send(msg []byte) {
+	session.WriteMessage(websocket.BinaryMessage, msg)
+}
+
+func (session *clientSession) dispatchReceivedMessage() error {
+	_, data, err := session.ReadMessage()
+	if err != nil {
+		return err
+	}
+	session.api.DispatchMessageEvent(data)
+	return nil
+}
+
+func newDialer(url string) (*websocket.Conn, error) {
+	dialer := &websocket.Dialer{}
+	conn, _, err := dialer.Dial(strings.Replace(url, "http", "ws", 1), nil)
+	return conn, err
+}
+
+func newSession(url string, serializer *typhenapi.Serializer) (*clientSession, error) {
+	conn, connErr := newDialer(url)
+	if connErr != nil {
+		return nil, connErr
+	}
+	session := &clientSession{conn, nil}
+	session.api = api.New(session, serializer, nil)
+	return session, nil
+}
 
 func newTestServer() (*httptest.Server, *io.PipeWriter) {
 	main.Log.Level = logrus.WarnLevel
@@ -23,30 +57,46 @@ func newTestServer() (*httptest.Server, *io.PipeWriter) {
 	return server, logWriter
 }
 
-func newDialer(url string) (*websocket.Conn, error) {
-	dialer := &websocket.Dialer{}
-	conn, _, err := dialer.Dial(strings.Replace(url, "http", "ws", 1), nil)
-	return conn, err
-}
+func TestMain(t *testing.T) {
+	Convey("main", t, func() {
+		Convey("BattleServer", func() {
+			server, logWriter := newTestServer()
+			serializer := typhenapi.NewJSONSerializer()
 
-func TestNewEngine(t *testing.T) {
-	server, logWriter := newTestServer()
-	defer logWriter.Close()
-	defer server.Close()
+			Convey("should be connectable by web socket protocol", func(c C) {
+				done := make(chan bool)
+				go func() {
+					conn, connErr := newDialer(server.URL + "/room/1")
+					defer conn.Close()
+					c.So(connErr, ShouldBeNil)
+					done <- true
+				}()
+				_ = <-done
+			})
 
-	conn, connErr := newDialer(server.URL + "/room/1")
-	defer conn.Close()
-	assert.NoError(t, connErr)
+			Convey("should respond to a ping message", func(c C) {
+				done := make(chan bool)
+				go func() {
+					session, _ := newSession(server.URL+"/room/1", serializer)
+					defer session.Close()
 
-	serializer := typhenapi.NewJSONSerializer()
-	ping := &battle.Ping{"Foobar"}
-	message, messageErr := typhenapi.NewMessage(serializer, battleapi.MessageType_Ping, ping)
-	assert.NoError(t, messageErr)
-	conn.WriteMessage(websocket.BinaryMessage, message.Bytes())
+					session.api.Battle.Ping = func(p *battle.Ping) {
+						c.So(p.Message, ShouldEqual, "Foobar")
+						done <- true
+					}
 
-	messageType, received, readMessageErr := conn.ReadMessage()
-	assert.NoError(t, readMessageErr)
-	assert.Equal(t, websocket.BinaryMessage, messageType)
+					ping := &battle.Ping{"Foobar"}
+					session.api.Battle.SendPing(ping)
+					err := session.dispatchReceivedMessage()
+					c.So(err, ShouldBeNil)
+				}()
+				_ = <-done
+			})
 
-	assert.Equal(t, message.Bytes(), received)
+			Reset(func() {
+				server.Close()
+				logWriter.Close()
+			})
+		})
+	})
 }
