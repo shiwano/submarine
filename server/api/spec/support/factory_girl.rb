72e84550bd675fe1acd90aea1bcb8425a9d92173
Shogo Iwano <shiwano@gmail.com>
2015-12-28 14:58:12 +0900

    api: Remove DatabaseCleaner code from spec/support/factory_girl.rb

--- server/api/spec/support/factory_girl.rb
+++ server/api/spec/support/factory_girl.rb
@@ -4,11 +4,6 @@ RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
 
   config.before(:suite) do
-    begin
-      DatabaseCleaner.start
-      FactoryGirl.lint
-    ensure
-      DatabaseCleaner.clean
-    end
+    FactoryGirl.lint
   end
 end
