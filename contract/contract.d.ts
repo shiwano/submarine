84074066589157980881866f2d8634d960ad34bd
Shogo Iwano <shiwano@gmail.com>
2015-12-28 12:23:41 +0900

    contract: Add definitions for Room

--- contract/contract.d.ts
+++ contract/contract.d.ts
@@ -17,14 +17,24 @@ declare module Submarine {
     name: string;
   }
 
+  interface Room {
+    id: integer;
+    battleServerBaseUri: string;
+    members: User[];
+  }
+
   /** @noAuthRequired */
   function ping(message: string): { message: string; };
   /** @noAuthRequired */
   function signUp(name: string, password: string): { user: User; };
   /** @noAuthRequired */
-  function login(name: string, password: string): { user: User; };
+  function login(name: string, password: string): { user: User; joinedRoom?: Room; };
 
   function findUser(name: string): { user?: User; };
+
+  function createRoom(): { room: Room; };
+  function getRooms(): { rooms: Room[]; }
+  function joinIntoRoom(room_id: integer): void;
 
   module Battle {
     var ping: { message: string; }
---
+++ contract/server_contract.d.ts
@@ -0,0 +1,18 @@
+/// <reference path='./contract.d.ts' />
+
+declare module Submarine {
+  module Battle {
+    interface RoomMember {
+      roomKey: string;
+      user: User;
+    }
+
+    interface Room {
+      id: integer;
+      members: RoomMember[];
+    }
+
+    function findRoom(room_id: integer): { room?: Room; }
+    function closeRoom(room_id: integer): void;
+  }
+}
