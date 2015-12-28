b2fb25aa6a063f35e804c38465c620e3eab20396
Shogo Iwano <shiwano@gmail.com>
2015-12-28 23:00:37 +0900

    api: Fix create_room controller spec

--- server/api/spec/controllers/create_room_controller_spec.rb
+++ server/api/spec/controllers/create_room_controller_spec.rb
@@ -8,12 +8,13 @@ RSpec.describe CreateRoomController, type: :controller do
     end
 
     describe 'new_room' do
-      subject { @controller.new_room }
-
-      it { should be_a_kind_of(Room) }
+      it 'should return a new room' do
+        post :service
+        expect(@controller.new_room).to be_a_kind_of Room
+      end
       it 'should receive current_user#create_room' do
-        expect(@controller.current_user).to receive(:create_room)
-        subject
+        expect(@controller.current_user).to receive(:create_room).and_call_original
+        post :service
       end
     end
   end
