73af7ed2d0d742bf25f05749f0ff0c6f6f367428
Shogo Iwano <shiwano@gmail.com>
2015-12-28 20:23:43 +0900

    api: Refactor room model spec

--- server/api/spec/models/room_spec.rb
+++ server/api/spec/models/room_spec.rb
@@ -1,35 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe Room, type: :model do
-  subject { create(:room) }
+  let(:room) { create(:room) }
+  subject { room }
 
   it { should have_many(:room_members).dependent(:delete_all) }
   it { should have_many :users }
 
   describe '#join' do
     let(:user) { create(:user) }
+    subject { room.join(user) }
 
-    it 'should join user' do
-      expect { subject.join(user) }.to change { subject.users.count }.from(0).to(1)
+    it 'should join an user' do
+      expect { subject }.to change { room.users.count }.from(0).to(1)
     end
 
     context 'when the room is full' do
       before do
-        subject.max_member_count.times do
+        room.max_member_count.times do
           user = create(:user)
-          subject.join(user)
+          room.join(user)
         end
       end
-
-      it 'should raise the error' do
-        expect { subject.join(user) }.to raise_error ApplicationError::RoomIsFull
+      it 'should raise error' do
+        expect { subject }.to raise_error ApplicationError::RoomIsFull
       end
     end
   end
 
   describe '#to_api_type' do
-    it 'should return an instance of the typhen api type' do
-      expect(subject.to_api_type).to be_a_kind_of TyphenApi::Model::Submarine::Room
-    end
+    subject { room.to_api_type }
+    it { should be_a_kind_of TyphenApi::Model::Submarine::Room }
   end
 end
