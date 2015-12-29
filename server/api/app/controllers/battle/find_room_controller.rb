e7684d77551d4d3a1da02594dfd2df0a7426a103
Shogo Iwano <shiwano@gmail.com>
2015-12-29 22:37:07 +0900

    api: Implement battle/find_room api

--- server/api/app/controllers/battle/find_room_controller.rb
+++ server/api/app/controllers/battle/find_room_controller.rb
@@ -3,7 +3,10 @@ class Battle::FindRoomController < ApplicationController
   include TyphenApiRespondable
 
   def service
-    # Implement here.
-    render_response(room: nil)
+    render_response(room: target_room.try(:to_battle_room_api_type))
+  end
+
+  def target_room
+    @target_room ||= Room.find_by(id: params.room_id)
   end
 end
--- server/api/spec/requests/battle/find_room_spec.rb
+++ server/api/spec/requests/battle/find_room_spec.rb
@@ -2,5 +2,28 @@ require 'rails_helper'
 
 RSpec.describe "Battle::FindRoom", type: :request do
   describe "POST /battle/find_room" do
+    let(:room) { create(:room) }
+    let(:params) { { room_id: room.id } }
+
+    before do
+      post(battle_find_room_path, params)
+    end
+
+    context 'with a valid request' do
+      it 'should work' do
+        expect(response).to have_http_status(200)
+      end
+      it 'should return the requested room' do
+        expect(response_json[:room][:id]).to eq params[:room_id]
+      end
+    end
+
+    context 'with a no-existing room_id' do
+      let(:params) { { room_id: -1 } }
+
+      it 'should return nil as the requested room' do
+        expect(response_json[:room]).to eq nil
+      end
+    end
   end
 end
