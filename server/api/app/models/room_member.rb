d4cf193a278e8e48ee4c33a48d1a7b46ca1db4d8
Shogo Iwano <shiwano@gmail.com>
2015-12-29 09:22:10 +0900

    api: rake db:migrate

--- server/api/app/models/room_member.rb
+++ server/api/app/models/room_member.rb
@@ -7,6 +7,7 @@
 #  room_id    :integer
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
+#  room_key   :string(255)      not null
 #
 # Indexes
 #
--- server/api/db/schema.rb
+++ server/api/db/schema.rb
@@ -11,13 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151228154515) do
+ActiveRecord::Schema.define(version: 20151229001739) do
 
   create_table "room_members", force: :cascade do |t|
     t.integer  "user_id",    limit: 4
     t.integer  "room_id",    limit: 4
-    t.datetime "created_at",           null: false
-    t.datetime "updated_at",           null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.string   "room_key",   limit: 255, null: false
   end
 
   add_index "room_members", ["room_id"], name: "index_room_members_on_room_id", using: :btree
