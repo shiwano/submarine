ac0e12fcef6135a9b603dd3017e0749fb7129640
Shogo Iwano <shiwano@gmail.com>
2016-01-03 09:38:40 +0900

    battle: Add Respondable

---
+++ server/battle/src/app/respondable.go
@@ -0,0 +1,18 @@
+package main
+
+// Respondable is the general-purpose respondable type for channel.
+type Respondable struct {
+	value    interface{}
+	response interface{}
+	done     chan interface{}
+	err      error
+}
+
+func newRespondable(value interface{}) *Respondable {
+	return &Respondable{value: value, done: make(chan interface{})}
+}
+
+func (r *Respondable) wait() error {
+	r.response = <-r.done
+	return r.err
+}
