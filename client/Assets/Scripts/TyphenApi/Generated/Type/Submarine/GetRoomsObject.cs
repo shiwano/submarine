b6b280611b1cc8cbba741ae7f11aaab6dbd8bf07
Shogo Iwano <shiwano@gmail.com>
2016-01-04 23:22:53 +0900

    tools: robo gen

--- client/Assets/Scripts/TyphenApi/Generated/Type/Submarine/GetRoomsObject.cs
+++ client/Assets/Scripts/TyphenApi/Generated/Type/Submarine/GetRoomsObject.cs
@@ -6,7 +6,7 @@ namespace TyphenApi.Type.Submarine
 {
     public partial class GetRoomsObject : TyphenApi.TypeBase<GetRoomsObject>
     {
-        protected static readonly SerializationInfo<GetRoomsObject, List<Room>> rooms = new SerializationInfo<GetRoomsObject, List<Room>>("rooms", false, (x) => x.Rooms, (x, v) => x.Rooms = v);
-        public List<Room> Rooms { get; set; }
+        protected static readonly SerializationInfo<GetRoomsObject, List<TyphenApi.Type.Submarine.Room>> rooms = new SerializationInfo<GetRoomsObject, List<TyphenApi.Type.Submarine.Room>>("rooms", false, (x) => x.Rooms, (x, v) => x.Rooms = v);
+        public List<TyphenApi.Type.Submarine.Room> Rooms { get; set; }
     }
 }
