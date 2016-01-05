253716fe61e0af819947f5700095a06f1b16d57b
Shogo Iwano <shiwano@gmail.com>
2016-01-06 08:25:42 +0900

    client: Add LoginApi service

---
+++ client/Assets/Scripts/Common/Services/LoginApi.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Text.RegularExpressions;
+using UniRx;
+using Zenject;
+using Type = TyphenApi.Type.Submarine;
+
+namespace Submarine.Services
+{
+    public class LoginApi
+    {
+        public struct Result
+        {
+            public readonly string SessionKey;
+            public readonly Type.LoggedInUser LoggedInUser;
+
+            public Result(string sessionKey, Type.LoggedInUser loggedInUser)
+            {
+                SessionKey = sessionKey;
+                LoggedInUser = loggedInUser;
+            }
+        }
+
+        const string SessionKeyPattern = @"_submarine_api_session=([a-zA-Z0-9]+);";
+
+        [Inject]
+        TyphenApi.WebApi.Submarine webApi;
+
+        public IObservable<Result> Login(string userName, string passWord)
+        {
+            return webApi.Login(userName, passWord)
+                .Send()
+                .Select(r =>
+                {
+                    var sessionKey = GetSessionKeyFromCookie(r.Headers["Set-Cookie"]);
+                    return new Result(sessionKey, r.Data.User);
+                });
+        }
+
+        public IObservable<Result> SignUp(string userName, string passWord)
+        {
+            return webApi.SignUp(userName, passWord)
+                .Send()
+                .Select(r =>
+                {
+                    var sessionKey = GetSessionKeyFromCookie(r.Headers["Set-Cookie"]);
+                    return new Result(sessionKey, r.Data.User);
+                });
+        }
+
+        string GetSessionKeyFromCookie(string cookie)
+        {
+            var match = Regex.Match(cookie, SessionKeyPattern, RegexOptions.Multiline);
+            if (match == null)
+            {
+                throw new InvalidOperationException("No session key!");
+            }
+            return match.Groups[1].Value;
+        }
+    }
+}
