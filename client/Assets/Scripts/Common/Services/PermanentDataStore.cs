c17cc035c9ee6124473327f21cc213dd57fa6a6e
Shogo Iwano <shiwano@gmail.com>
2016-01-06 08:25:16 +0900

    client: Add PermanentDataStore service

---
+++ client/Assets/Scripts/Common/Services/PermanentDataStore.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Submarine.Services
+{
+    public class PermanentDataStore
+    {
+        public string ApiSessionKey
+        {
+            get { return PlayerPrefs.GetString("ApiSessionKey"); }
+            set { PlayerPrefs.SetString("ApiSessionKey", value); }
+        }
+
+        public string UserName
+        {
+            get { return PlayerPrefs.GetString("UserName"); }
+            set { PlayerPrefs.SetString("UserName", value); }
+        }
+
+        public string Password
+        {
+            get { return PlayerPrefs.GetString("UserName"); }
+            set { PlayerPrefs.SetString("UserName", value); }
+        }
+
+        public void Save()
+        {
+            PlayerPrefs.Save();
+        }
+
+        public void Clear()
+        {
+            PlayerPrefs.DeleteAll();
+        }
+    }
+}
