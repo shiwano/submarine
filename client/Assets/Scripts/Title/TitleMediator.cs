2b5f17f8746a472797cf3bc82e6069c88a14b236
Shogo Iwano <shiwano@gmail.com>
2016-01-07 22:56:05 +0900

    client: Use login, signup commands in TitleMediator

--- client/Assets/Scripts/Title/TitleMediator.cs
+++ client/Assets/Scripts/Title/TitleMediator.cs
@@ -1,28 +1,49 @@
-﻿using UnityEngine;
-using UniRx;
+﻿using UniRx;
 using Zenject;
 
 namespace Submarine
 {
     public class TitleMediator : IInitializable
     {
+        readonly Services.PermanentDataStore dataStore;
+        readonly Commands.Login loginCommand;
+        readonly Commands.SignUp signUpCommand;
         readonly Commands.SceneChange sceneChangeCommand;
+        readonly TitleEvent titleEvent;
         readonly TitleView view;
 
         public TitleMediator(
+            Services.PermanentDataStore dataStore,
+            Commands.Login loginCommand,
+            Commands.SignUp signUpCommand,
             Commands.SceneChange sceneChangeCommand,
+            TitleEvent titleEvent,
             TitleView view)
         {
-            this.view = view;
+            this.dataStore = dataStore;
+            this.loginCommand = loginCommand;
+            this.signUpCommand = signUpCommand;
             this.sceneChangeCommand = sceneChangeCommand;
+            this.titleEvent = titleEvent;
+            this.view = view;
         }
 
         public void Initialize()
         {
-            view.StartButtonClickedAsObservable().Subscribe(_ =>
+            titleEvent.LoginSucceeded.AddListener(OnLoginSuccess);
+            view.StartButtonClickedAsObservable().Take(1).Subscribe(_ => OnStartButtonClick());
+        }
+
+        void OnStartButtonClick()
+        {
+            if (dataStore.HasLoginData)
             {
-                Debug.Log("a");
-            });
+                loginCommand.Execute();
+            }
+            else
+            {
+                signUpCommand.Execute("Test");
+            }
         }
 
         void OnLoginSuccess()
