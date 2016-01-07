119d0b09c91b96d246b03a8a8c1a8f22baaf105d
Shogo Iwano <shiwano@gmail.com>
2016-01-07 23:59:01 +0900

    client: Add Logging errors of WebAPI

--- client/Assets/Scripts/TyphenApi/WebApi/Submarine.cs
+++ client/Assets/Scripts/TyphenApi/WebApi/Submarine.cs
@@ -1,3 +1,4 @@
+using UnityEngine;
 using TyphenApi.Type.Submarine;
 
 namespace TyphenApi.WebApi
@@ -16,14 +17,23 @@ namespace TyphenApi.WebApi
         public override void OnBeforeRequestSend(IWebApiRequest request)
         {
             request.Headers["Content-Type"] = "application/json";
+            #if UNITY_EDITOR
+            Debug.Log(request);
+            #endif
         }
 
         public override void OnRequestError(IWebApiRequest request, WebApiError<Error> error)
         {
+            #if UNITY_EDITOR
+            Debug.LogError(error);
+            #endif
         }
 
         public override void OnRequestSuccess(IWebApiRequest request, IWebApiResponse response)
         {
+            #if UNITY_EDITOR
+            Debug.Log(response);
+            #endif
         }
     }
 }
