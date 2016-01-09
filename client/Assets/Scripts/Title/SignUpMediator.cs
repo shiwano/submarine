9e462970b851491037ab609540d1b60b2d3ce6e1
Shogo Iwano <shiwano@gmail.com>
2016-01-09 20:25:16 +0900

    client: Add SignUpMediator and SignUpView

---
+++ client/Assets/Scripts/Title/SignUpMediator.cs
@@ -0,0 +1,36 @@
+﻿using UniRx;
+using Zenject;
+
+namespace Submarine.Title
+{
+    public class SignUpMediator : IInitializable
+    {
+        [Inject]
+        TitleEvents events;
+        [Inject]
+        SignUpCommand signUpCommand;
+        [Inject]
+        SignUpView view;
+
+        public void Initialize()
+        {
+            events.SignUpStarted.AddListener(OnSignUpStart);
+            view.SignUpButtonClickedAsObservable().Subscribe(_ => OnSignUpButtonClick()).AddTo(view);
+            view.Hide();
+        }
+
+        public void OnSignUpStart()
+        {
+            view.Show();
+            view.FocusToNameInputField();
+        }
+
+        public void OnSignUpButtonClick()
+        {
+            if (!string.IsNullOrEmpty(view.InputtedName))
+            {
+                signUpCommand.Execute(view.InputtedName);
+            }
+        }
+    }
+}
---
+++ client/Assets/Scripts/Title/SignUpView.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace Submarine.Title
+{
+    public class SignUpView : MonoBehaviour
+    {
+        [SerializeField]
+        Button signUpButton;
+        [SerializeField]
+        InputField nameInputField;
+
+        public string InputtedName { get { return nameInputField.text; } }
+
+        public IObservable<Unit> SignUpButtonClickedAsObservable()
+        {
+            return signUpButton.onClickAsObservableWithThrottle();
+        }
+
+        public IObservable<string> NameChangedAsObservable()
+        {
+            return nameInputField.OnValueChangeAsObservable();
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+        }
+
+        public void Hide()
+        {
+            gameObject.SetActive(false);
+        }
+
+        public void FocusToNameInputField()
+        {
+            nameInputField.ActivateInputField();
+            nameInputField.Select();
+        }
+    }
+}
