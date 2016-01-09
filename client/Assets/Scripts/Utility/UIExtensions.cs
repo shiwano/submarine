b61e3f6bfdb4f85cf2b3e9f6b66ffb7ada43210a
Shogo Iwano <shiwano@gmail.com>
2016-01-09 12:47:19 +0900

    client: Add UIExtensions

---
+++ client/Assets/Scripts/Utility/UIExtensions.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine.UI;
+using System;
+using UniRx;
+
+namespace Submarine
+{
+    public static class UIExtensions
+    {
+        public static IObservable<Unit> onClickAsObservableWithThrottle(this Button button, TimeSpan? dueTime = null)
+        {
+            // TODO: Add ThrottleFirst operator after UniRX updating.
+            return button.OnClickAsObservable();
+        }
+    }
+}
