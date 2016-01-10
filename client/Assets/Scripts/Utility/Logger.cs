a48c310281b38dd7e433368b7172968f2c8cec8f
Shogo Iwano <shiwano@gmail.com>
2016-01-11 02:28:19 +0900

    client: Fix logging of null arguments

--- client/Assets/Scripts/Utility/Logger.cs
+++ client/Assets/Scripts/Utility/Logger.cs
@@ -7,27 +7,31 @@ namespace Submarine
     {
         public static void Log(params object[] messages)
         {
-            UnityEngine.Debug.Log(BuildMessage(messages));
+            Debug.Log(BuildMessage(messages));
         }
 
         public static void LogWithColor(Color color, params object[] messages)
         {
-            UnityEngine.Debug.Log(BuildMessage(messages, color));
+            Debug.Log(BuildMessage(messages, color));
         }
 
         public static void LogError(params object[] messages)
         {
-            UnityEngine.Debug.LogError(BuildMessage(messages, Color.red));
+            Debug.LogError(BuildMessage(messages, Color.red));
         }
 
-        static string BuildMessage(object[] messages, Color? color = null)
+        static string BuildMessage(object[] messageObjs, Color? color = null)
         {
-            return string.Join("\n", messages.Select(message =>
-            {
-                return color != null ?
-                    Colorize(message.ToString(), color.Value) :
-                    message.ToString();
-            }).ToArray());
+            var messages = messageObjs
+                .Where(m => m != null)
+                .Select(messageObj =>
+                {
+                    return color != null ?
+                        Colorize(messageObj.ToString(), color.Value) :
+                        messageObj.ToString();
+                })
+                .ToArray();
+            return string.Join("\n", messages);
         }
 
         static string Colorize(string str, Color color)
