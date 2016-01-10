fa92e82bfb3b7436b32b8fcf2ffb04c5e19b6beb
Shogo Iwano <shiwano@gmail.com>
2016-01-10 15:23:43 +0900

    client: Add RoomService

---
+++ client/Assets/Scripts/Lobby/Services/RoomService.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using UniRx;
+using Zenject;
+using Type = TyphenApi.Type.Submarine;
+
+namespace Submarine.Lobby
+{
+    public class RoomService
+    {
+        [Inject]
+        TyphenApi.WebApi.Submarine webApi;
+
+        public IObservable<Type.JoinedRoom> CreateRoom()
+        {
+            return webApi.CreateRoom().Send().Select(r => r.Data.Room);
+        }
+
+        public IObservable<List<Type.Room>> GetRooms()
+        {
+            return webApi.GetRooms().Send().Select(r => r.Data.Rooms);
+        }
+
+        public IObservable<Type.JoinedRoom> JoinIntoRoom(Type.Room room)
+        {
+            return webApi.JoinIntoRoom(room.Id).Send().Select(r => r.Data.Room);
+        }
+    }
+}
