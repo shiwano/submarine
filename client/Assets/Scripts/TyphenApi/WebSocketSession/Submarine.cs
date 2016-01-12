1aabdf44d0e3d5bf40c711e614f2b89a116ae88b
Shogo Iwano <shiwano@gmail.com>
2016-01-13 00:39:01 +0900

    client: Add IsConnected to WebSocketSession

--- client/Assets/Scripts/TyphenApi/WebSocketSession/Submarine.cs
+++ client/Assets/Scripts/TyphenApi/WebSocketSession/Submarine.cs
@@ -1,15 +1,20 @@
 using TyphenApi.Type.Submarine;
 using WebSocketSharp;
+using UniRx;
 
 namespace TyphenApi.WebSocketSession
 {
     public class Submarine : Base.Submarine
     {
+        public readonly ReactiveProperty<bool> IsConnected;
+
         public Submarine(string requestUri) : base(requestUri)
         {
             var jsonSerializer = new JSONSerializer();
             MessageSerializer = jsonSerializer;
             MessageDeserializer = jsonSerializer;
+
+            IsConnected = new ReactiveProperty<bool>();
         }
 
         public override void OnConnectionCreate(WebSocket connection)
@@ -18,10 +23,12 @@ namespace TyphenApi.WebSocketSession
 
         public override void OnConnectionOpen()
         {
+            IsConnected.Value = true;
         }
 
         public override void OnConnectionClose(ushort code, string reason, bool wasClean)
         {
+            IsConnected.Value = false;
         }
 
         public override void OnBeforeMessageSend(TyphenApi.TypeBase message)
