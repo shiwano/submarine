0764042923e9ecb49bf166f6520ed94824bd5dde
Shogo Iwano <shiwano@gmail.com>
2016-01-13 07:41:32 +0900

    battle: Create StartBattleCommand

---
+++ client/Assets/Scripts/Lobby/Commands/StartBattleCommand.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Zenject;
+using Zenject.Commands;
+using Type = TyphenApi.Type.Submarine;
+
+namespace Submarine.Lobby
+{
+    public class StartBattleCommand : Command<Type.JoinedRoom>
+    {
+        public class Handler : ICommandHandler<Type.JoinedRoom>
+        {
+            [Inject]
+            BattleService battleService;
+
+            public void Execute(Type.JoinedRoom room)
+            {
+                var baseUri = new Uri(room.BattleServerBaseUri);
+                var relativeUri = string.Format("rooms/{0}?room_key={1}", room.Id, room.RoomKey);
+                battleService.Start(new Uri(baseUri, relativeUri).ToString());
+            }
+        }
+    }
+}
