5473dee1bf53ab3d0856181e9edd8408cf6c51ab
Shogo Iwano <shiwano@gmail.com>
2016-01-13 08:23:26 +0900

    client: Ping to battle server

--- client/Assets/Scripts/Lobby/LobbyMediator.cs
+++ client/Assets/Scripts/Lobby/LobbyMediator.cs
@@ -1,16 +1,37 @@
-﻿using Zenject;
+﻿using UniRx;
+using Zenject;
 
 namespace Submarine.Lobby
 {
     public class LobbyMediator : IInitializable
     {
         [Inject]
-        LobbyEvents events;
+        LobbyModel lobbyModel;
+        [Inject]
+        BattleService battleService;
+        [Inject]
+        StartBattleCommand startBattleCommand;
         [Inject]
         LobbyView view;
 
         public void Initialize()
         {
+            lobbyModel.HasJoinedIntoRoom.Where(v => v).Take(1).Subscribe(_ => OnJoinIntoRoom()).AddTo(view);
+            battleService.IsStarted.Where(v => v).Take(1).Subscribe(_ => OnBattleStart()).AddTo(view);
+        }
+
+        void OnJoinIntoRoom()
+        {
+            startBattleCommand.Execute(lobbyModel.JoinedRoom.Value);
+        }
+
+        void OnBattleStart()
+        {
+            battleService.Api.OnPingReceiveAsObservable().Subscribe(message =>
+            {
+                Logger.Log(message.Message);
+            });
+            battleService.Api.SendPing("Hey");
         }
     }
 }
