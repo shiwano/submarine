9b417ba84c0d733b9ff376c2d651c05ab18f810a
Shogo Iwano <shiwano@gmail.com>
2016-01-28 07:50:43 +0900

    contract: Add Battle.find_room_member API

--- contract/server_contract.d.ts
+++ contract/server_contract.d.ts
@@ -4,16 +4,17 @@ declare module Submarine {
   module Battle {
     interface RoomMember extends User {
       id: integer;
-      roomKey: string;
+      roomId: integer;
     }
 
     interface Room {
       id: integer;
-      members: RoomMember[];
     }
 
     /** @noAuthRequired */
     function findRoom(room_id: integer): { room?: Room; }
+    /** @noAuthRequired */
+    function findRoomMember(room_key: string): { room_member?: RoomMember; }
     /** @noAuthRequired */
     function closeRoom(room_id: integer): void;
   }
