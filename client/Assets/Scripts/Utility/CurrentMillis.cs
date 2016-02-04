8daaf49db604e3dac45f894949f6a8c67a8eb33a
Shogo Iwano <shiwano@gmail.com>
2016-02-05 03:22:13 +0900

    client: Add CurrentMillis

---
+++ client/Assets/Scripts/Utility/CurrentMillis.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Submarine
+{
+    public static class CurrentMillis
+    {
+        static readonly DateTime UnixEpoch = new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc);
+
+        public static long Now
+        {
+            get { return FromDateTime(DateTime.UtcNow); }
+        }
+
+        public static DateTime FromMilliseconds(long ms)
+        {
+            return CurrentMillis.UnixEpoch.AddMilliseconds(ms).ToLocalTime();
+        }
+
+        public static long FromDateTime(DateTime dateTime)
+        {
+            return (long)(dateTime.ToUniversalTime() - CurrentMillis.UnixEpoch).TotalMilliseconds;
+        }
+    }
+}
