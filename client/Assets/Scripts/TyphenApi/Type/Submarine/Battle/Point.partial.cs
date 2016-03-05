c125ed1be303be52c75fdafbf9ac0e4ff1a59f8d
Shogo Iwano <shiwano@gmail.com>
2016-03-05 22:23:10 +0900

    client: Add Point patial class

---
+++ client/Assets/Scripts/TyphenApi/Type/Submarine/Battle/Point.partial.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace TyphenApi.Type.Submarine.Battle
+{
+    public partial class Point
+    {
+        public static Point FromVector2(Vector2 v)
+        {
+            return new Point()
+            {
+                X = v.x,
+                Y = v.y,
+            };
+        }
+
+        public Vector2 ToVector2()
+        {
+            return new Vector2((float)X, (float)Y);
+        }
+    }
+}
