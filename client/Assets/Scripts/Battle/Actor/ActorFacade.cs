0eefc0b1024a253345eacea5cd7d94031dfa1550
Shogo Iwano <shiwano@gmail.com>
2016-03-06 21:23:20 +0900

    client: Synchronize actor position

--- client/Assets/Scripts/Battle/Actor/ActorFacade.cs
+++ client/Assets/Scripts/Battle/Actor/ActorFacade.cs
@@ -17,7 +17,12 @@ namespace Submarine.Battle
         public Type.Battle.Actor Actor { get { return actor; } }
         public ActorMotor Motor { get { return motor; } }
         public ActorView View { get { return view; } }
-
         public bool IsMine { get { return actor.UserId == userModel.LoggedInUser.Value.Id; } }
+
+        public override void Tick()
+        {
+            base.Tick();
+            view.ActorPosition = motor.GetCurrentPosition();
+        }
     }
 }
--- client/Assets/Scripts/Battle/Actor/ActorView.cs
+++ client/Assets/Scripts/Battle/Actor/ActorView.cs
@@ -4,5 +4,12 @@ namespace Submarine.Battle
 {
     public abstract class ActorView : MonoBehaviour
     {
+        const float PositionY = 20f;
+
+        public Vector2 ActorPosition
+        {
+            get { return new Vector2(transform.position.x, transform.position.z); }
+            set { transform.position = new Vector3(value.x, PositionY, value.y); }
+        }
     }
 }
