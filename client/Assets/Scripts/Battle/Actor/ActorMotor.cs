cae8cc1965547c7985d1f441b1a840cd8a9494b8
Shogo Iwano <shiwano@gmail.com>
2016-03-13 11:17:01 +0900

    client: Add Movement#NormalizedVelocity

--- client/Assets/Scripts/Battle/Actor/ActorMotor.cs
+++ client/Assets/Scripts/Battle/Actor/ActorMotor.cs
@@ -56,7 +56,6 @@ namespace Submarine.Battle
 
         Type.Battle.Movement movement;
         Accelerator accelerator;
-        Vector2 normalizedVelocity;
 
         DateTime ChangedAt
         {
@@ -72,19 +71,14 @@ namespace Submarine.Battle
         {
             this.movement = movement;
             accelerator = movement.Accelerator == null ?
-                null :
-                new Accelerator(movement.Accelerator);
-
-            var directionForClient = (float)movement.Direction;
-            normalizedVelocity = new Vector2(
-                Mathf.Cos(directionForClient * Mathf.Deg2Rad),
-                Mathf.Sin(directionForClient * Mathf.Deg2Rad)
-            );
+                null : new Accelerator(movement.Accelerator);
         }
 
         public Quaternion GetCurrentRotation()
         {
-            return Quaternion.LookRotation(normalizedVelocity);
+            return movement == null ?
+                Quaternion.identity :
+                Quaternion.LookRotation(movement.NormalizedVelocity);
         }
 
         public Vector2 GetCurrentPosition()
@@ -92,24 +86,24 @@ namespace Submarine.Battle
             if (movement == null) return Vector2.zero;
             if (accelerator == null) return movement.Position.ToVector2();
 
-            double s1, s2 = 0d;
+            double t1, t2 = 0d;
             if (battleModel.Now > accelerator.ReachedMaxSpeedAt)
             {
-                s1 = (accelerator.ReachedMaxSpeedAt - ChangedAt).TotalSeconds;
-                s2 = (battleModel.Now - accelerator.ReachedMaxSpeedAt).TotalSeconds;
+                t1 = (accelerator.ReachedMaxSpeedAt - ChangedAt).TotalSeconds;
+                t2 = (battleModel.Now - accelerator.ReachedMaxSpeedAt).TotalSeconds;
             }
             else
             {
-                s1 = (battleModel.Now - ChangedAt).TotalSeconds;
+                t1 = (battleModel.Now - ChangedAt).TotalSeconds;
             }
 
             var p = movement.Position.ToVector2();
-            var v = accelerator.StartSpeed * s1;
-            var a = accelerator.Acceleration * Math.Pow(s1, 2) / 2;
-            var d1 = normalizedVelocity * (float)(v + a);
+            var v = accelerator.StartSpeed * t1;
+            var a = accelerator.Acceleration * Math.Pow(t1, 2) / 2;
+            var d1 = movement.NormalizedVelocity * (float)(v + a);
             if (accelerator.IsAccelerating)
             {
-                var d2 = normalizedVelocity * (float)(accelerator.MaxSpeed * s2);
+                var d2 = movement.NormalizedVelocity * (float)(accelerator.MaxSpeed * t2);
                 return p + d1 + d2;
             }
             return p + d1;
---
+++ client/Assets/Scripts/TyphenApi/Type/Submarine/Battle/Movement.partial.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace TyphenApi.Type.Submarine.Battle
+{
+    public partial class Movement
+    {
+        Vector2? normalizedVelocity;
+        public Vector2 NormalizedVelocity
+        {
+            get
+            {
+                if (!normalizedVelocity.HasValue)
+                {
+                    var directionForClient = (float)Direction;
+                    normalizedVelocity = new Vector2(
+                        Mathf.Cos(directionForClient * Mathf.Deg2Rad),
+                        Mathf.Sin(directionForClient * Mathf.Deg2Rad)
+                    );
+                }
+                return normalizedVelocity.Value;
+            }
+        }
+    }
+}
