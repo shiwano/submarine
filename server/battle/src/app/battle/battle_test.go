8e28449141b17d0feea6973eb599f2bfde2dbc19
Shogo Iwano <shiwano@gmail.com>
2016-03-18 09:15:49 +0900

    battle: Add Battle#EnterUser test

--- server/battle/src/app/battle/battle_test.go
+++ server/battle/src/app/battle/battle_test.go
@@ -10,13 +10,11 @@ func TestBattle(t *testing.T) {
 	Convey("Battle", t, func() {
 		battle := New(60 * time.Second)
 
-		Convey("#CreateSubmarineUnlessExists", func() {
-			Convey("when the submarine does not exist", func() {
-				Convey("should not create the submarine", func() {
-					battle.EnterUser(1)
-					submarine := battle.context.SubmarineByUserID(1)
-					So(submarine, ShouldNotBeNil)
-				})
+		Convey("#EnterUser", func() {
+			Convey("should create the submarine", func() {
+				battle.EnterUser(1)
+				submarine := battle.context.SubmarineByUserID(1)
+				So(submarine, ShouldNotBeNil)
 			})
 
 			Convey("when the submarine already exists", func() {
@@ -26,6 +24,15 @@ func TestBattle(t *testing.T) {
 				Convey("should not replace the existing with new submarine instance", func() {
 					battle.EnterUser(1)
 					So(submarine, ShouldEqual, battle.context.SubmarineByUserID(1))
+				})
+			})
+
+			Convey("when the battle already started", func() {
+				battle.IsStarted = true
+
+				Convey("should send to reenterUserCh", func() {
+					battle.EnterUser(1)
+					So(len(battle.reenterUserCh), ShouldEqual, 1)
 				})
 			})
 		})
