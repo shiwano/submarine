22348459d67192b7a043fae0896d1179e949e26b
Shogo Iwano <shiwano@gmail.com>
2016-05-18 01:22:33 +0900

    client: Remove unused method

--- client/Assets/Editor/NavMesh/NavMeshPostprocessor.cs
+++ client/Assets/Editor/NavMesh/NavMeshPostprocessor.cs
@@ -39,9 +39,4 @@ public class NavMeshPostprocessor : AssetPostprocessor
         AssetDatabase.Refresh();
         Debug.Log("Generated " + outputPath);
     }
-
-    string CombinePaths(string path1, string path2)
-    {
-        return Path.Combine(path1, path2).Replace("\\", "/");
-    }
 }
