d31e85395ccf588d17d99ffb8ba0851e87fc8b42
Shogo Iwano <shiwano@gmail.com>
2016-05-31 05:31:53 +0900

    client: Add StartBattleCommand

---
+++ client/Assets/Scripts/Battle/Commands/StartBattleCommand.cs
@@ -0,0 +1,26 @@
+﻿using Zenject;
+using Zenject.Commands;
+
+namespace Submarine.Battle
+{
+    public class StartBattleCommand : Command
+    {
+        public class Handler : ICommandHandler
+        {
+            [Inject]
+            BattleService battleService;
+            [Inject]
+            BattleModel battleModel;
+
+            public void Execute()
+            {
+                if (battleModel.State.Value != BattleState.InPreparation)
+                {
+                    Logger.LogError("Can't start the battle");
+                    return;
+                }
+                battleService.Api.SendStartRequest();
+            }
+        }
+    }
+}
