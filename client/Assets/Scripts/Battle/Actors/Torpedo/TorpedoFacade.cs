8f8ecc18d54d05df61d485e3ef7b8218eb98d80c
Shogo Iwano <shiwano@gmail.com>
2016-06-03 07:31:40 +0900

    client: Add torpedo scripts

---
+++ client/Assets/Scripts/Battle/Actors/Torpedo/TorpedoFacade.cs
@@ -0,0 +1,13 @@
+﻿using Zenject;
+using Type = TyphenApi.Type.Submarine;
+
+namespace Submarine.Battle
+{
+    public class TorpedoFacade : ActorFacade
+    {
+        public class Factory : FacadeFactory<Type.Battle.Actor, TorpedoFacade> { }
+
+        [Inject]
+        TorpedoView view;
+    }
+}
---
+++ client/Assets/Scripts/Battle/Actors/Torpedo/TorpedoView.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using DG.Tweening;
+
+namespace Submarine.Battle
+{
+    public class TorpedoView : ActorView
+    {
+        [SerializeField]
+        GameObject model;
+        [SerializeField]
+        GameObject explosionEffectPrefab;
+
+        public override void ChangeToEnemyColor() { }
+
+        void OnDestroy()
+        {
+            Instantiate(explosionEffectPrefab, transform.position, transform.rotation);
+        }
+    }
+}
