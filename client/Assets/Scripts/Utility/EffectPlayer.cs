ecef0019fe1b24519370aa9ab1be8e7010cc7e2f
Shogo Iwano <shiwano@gmail.com>
2016-06-21 09:16:39 +0900

    client: Rename EffectSweeper -> EffectPlayer

--- client/Assets/Scripts/Utility/EffectPlayer.cs
+++ client/Assets/Scripts/Utility/EffectPlayer.cs
@@ -4,11 +4,16 @@ using UniRx;
 
 namespace Submarine
 {
-    public class EffectSweeper : MonoBehaviour
+    public class EffectPlayer : MonoBehaviour
     {
+        [SerializeField]
+        GameObject effectPrefab;
+
         void Start()
         {
-            var particle = GetComponent<ParticleSystem>();
+            var effect = Instantiate(effectPrefab);
+            effect.transform.SetParent(transform, false);
+            var particle = effect.GetComponent<ParticleSystem>();
 
             Observable.Interval(TimeSpan.FromSeconds(particle.duration))
                 .Take(1)
