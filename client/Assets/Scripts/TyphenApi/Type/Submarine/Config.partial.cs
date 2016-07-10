9d8320ca5997fa5076bb57ffa861cc006c7f5094
Shogo Iwano <shiwano@gmail.com>
2016-07-10 17:07:13 +0900

    client: Remove Config.local.json

--- client/Assets/Scripts/TyphenApi/Type/Submarine/Config.partial.cs
+++ client/Assets/Scripts/TyphenApi/Type/Submarine/Config.partial.cs
@@ -4,16 +4,12 @@ namespace TyphenApi.Type.Submarine
 {
     public partial class Config
     {
-        #if UNITY_EDITOR
-        public const string Path = "Config/Config.local";
-        #else
         public const string Path = "Config/Config";
-        #endif
 
         public static Config Load()
         {
             var textAsset = Resources.Load<TextAsset>(Config.Path);
-            Debug.Assert(textAsset != null, "Not found a Config file");
+            Debug.Assert(textAsset != null, "Not found the Config file");
             return Config.FromJSON(textAsset.text);
         }
     }
