e41593e8ceab0a7fde71340b21b56315bf10e1c8
Shogo Iwano <shiwano@gmail.com>
2016-07-10 14:46:12 +0900

    contract: rake gen

--- client/Assets/Scripts/TyphenApi/Generated/Type/Submarine/Battle/Finish.cs
+++ client/Assets/Scripts/TyphenApi/Generated/Type/Submarine/Battle/Finish.cs
@@ -6,8 +6,8 @@ namespace TyphenApi.Type.Submarine.Battle
 {
     public partial class Finish : TyphenApi.TypeBase<Finish>
     {
-        protected static readonly SerializationInfo<Finish, long> winnerUserId = new SerializationInfo<Finish, long>("winner_user_id", false, (x) => x.WinnerUserId, (x, v) => x.WinnerUserId = v);
-        public long WinnerUserId { get; set; }
+        protected static readonly SerializationInfo<Finish, long?> winnerUserId = new SerializationInfo<Finish, long?>("winner_user_id", true, (x) => x.WinnerUserId, (x, v) => x.WinnerUserId = v);
+        public long? WinnerUserId { get; set; }
         protected static readonly SerializationInfo<Finish, long> finishedAt = new SerializationInfo<Finish, long>("finished_at", false, (x) => x.FinishedAt, (x, v) => x.FinishedAt = v);
         public long FinishedAt { get; set; }
     }
--- server/api/lib/typhen_api/typhen_api/model/submarine/battle/finish.rb
+++ server/api/lib/typhen_api/typhen_api/model/submarine/battle/finish.rb
@@ -4,7 +4,7 @@ module TyphenApi::Model::Submarine::Battle
   class Finish
     include Virtus.model(:strict => true)
 
-    attribute :winner_user_id, Integer, :required => true
+    attribute :winner_user_id, Integer, :required => false
     attribute :finished_at, Integer, :required => true
   end
 end
