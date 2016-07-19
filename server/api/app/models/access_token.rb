b59ad064ac6406403c1ddc8eaaca1f7ca52b34cd
Shogo Iwano <shiwano@gmail.com>
2016-07-20 05:20:19 +0900

    api: rails db:migrate

--- server/api/app/models/access_token.rb
+++ server/api/app/models/access_token.rb
@@ -1,3 +1,20 @@
+# == Schema Information
+#
+# Table name: access_tokens
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  token      :string(255)      not null
+#  expires_at :datetime         not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_access_tokens_on_token    (token) UNIQUE
+#  index_access_tokens_on_user_id  (user_id) UNIQUE
+#
+
 class AccessToken < ApplicationRecord
   belongs_to :user
 end
--- server/api/db/schema.rb
+++ server/api/db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719134146) do
+ActiveRecord::Schema.define(version: 20160719154025) do
+
+  create_table "access_tokens", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "user_id",    null: false
+    t.string   "token",      null: false
+    t.datetime "expires_at", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["token"], name: "index_access_tokens_on_token", unique: true, using: :btree
+    t.index ["user_id"], name: "index_access_tokens_on_user_id", unique: true, using: :btree
+  end
 
   create_table "room_members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "user_id"
@@ -41,6 +51,7 @@ ActiveRecord::Schema.define(version: 20160719134146) do
     t.index ["name"], name: "index_users_on_name", using: :btree
   end
 
+  add_foreign_key "access_tokens", "users"
   add_foreign_key "room_members", "rooms"
   add_foreign_key "room_members", "users"
 end
