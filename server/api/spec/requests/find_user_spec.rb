624fa03e2a8fa1bd8cca4182971f1c53fc7ab291
Shogo Iwano <shiwano@gmail.com>
2016-07-20 22:10:16 +0900

    api: Fix find_user API spec

--- server/api/spec/requests/find_user_spec.rb
+++ server/api/spec/requests/find_user_spec.rb
@@ -2,7 +2,7 @@ require 'rails_helper'
 
 RSpec.describe 'FindUser', type: :request do
   describe 'POST /find_user', with_login: true do
-    let(:params) { { name: 'Shimakaze' } }
+    let(:request_params) { { name: 'Shimakaze' } }
 
     before do
       create(:user, name: 'Shimakaze')
@@ -11,29 +11,29 @@ RSpec.describe 'FindUser', type: :request do
 
     context 'with a valid request' do
       before do
-        post(find_user_path, params: params)
+        post(find_user_path, params: request_params)
       end
 
       it 'should work' do
         expect(response).to have_http_status(200)
       end
       it 'should return a reasponse that includes a user' do
-        expect(response_json[:user][:name]).to eq 'Shimakaze'
+        expect(parsed_response.user.name).to eq 'Shimakaze'
       end
     end
 
-    context 'with invalid params' do
+    context 'with invalid request_params' do
       it 'should not work' do
         expect { post(find_user_path) }.to raise_error(Virtus::CoercionError)
       end
     end
 
     context 'with a no-existing user name' do
-      let(:params) { { name: 'Kaga' } }
+      let(:request_params) { { name: 'Kaga' } }
 
       it 'should return empty' do
-        post(find_user_path, params: params)
-        expect(response_json[:user]).to be_nil
+        post(find_user_path, params: request_params)
+        expect(parsed_response.user).to be_nil
       end
     end
   end
