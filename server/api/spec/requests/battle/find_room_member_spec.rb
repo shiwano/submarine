f40d4897146304c47c484463db5c5d33b8665c19
Shogo Iwano <shiwano@gmail.com>
2016-07-20 22:27:36 +0900

    api: Fix battle/find_room_member API spec

--- server/api/spec/requests/battle/find_room_member_spec.rb
+++ server/api/spec/requests/battle/find_room_member_spec.rb
@@ -3,10 +3,10 @@ require 'rails_helper'
 RSpec.describe "Battle::FindRoomMember", type: :request do
   describe "POST /battle/find_room_member" do
     let(:room_member) { create(:room_member) }
-    let(:params) { { room_key: room_member.room_key } }
+    let(:request_params) { { room_key: room_member.room_key } }
 
     before do
-      post(battle_find_room_member_path, params: params)
+      post battle_find_room_member_path, params: request_params
     end
 
     context 'with a valid request' do
@@ -14,15 +14,15 @@ RSpec.describe "Battle::FindRoomMember", type: :request do
         expect(response).to have_http_status(200)
       end
       it 'should return the requested room' do
-        expect(response_json[:room_member][:id]).to eq room_member.user.id
+        expect(parsed_response.room_member.id).to eq room_member.user.id
       end
     end
 
     context 'with a no-existing room_key' do
-      let(:params) { { room_key: "" } }
+      let(:request_params) { { room_key: "" } }
 
       it 'should return nil as the requested room' do
-        expect(response_json[:room_member]).to eq nil
+        expect(parsed_response.room_member).to eq nil
       end
     end
   end
