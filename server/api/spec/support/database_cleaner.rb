0c899c65517bf1d68a51b80ac71f9cf9a5cab871
Shogo Iwano <shiwano@gmail.com>
2016-07-20 22:35:06 +0900

    api: Remove unused gems and code

--- server/api/spec/support/database_cleaner.rb
+++ server/api/spec/support/database_cleaner.rb
@@ -2,7 +2,6 @@ require 'database_cleaner'
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner[:redis].db = "redis://#{Settings.redis[:host]}:#{Settings.redis[:port]}/#{Settings.redis[:db]}"
     DatabaseCleaner[:active_record].strategy = :transaction
     DatabaseCleaner.clean_with :truncation
   end
