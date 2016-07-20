de97ced27d6e07684cfd0f37e75483e4db43538c
Shogo Iwano <shiwano@gmail.com>
2016-07-20 22:24:02 +0900

    api: Fix battle/close_room API spec

--- server/api/spec/requests/battle/close_room_spec.rb
+++ server/api/spec/requests/battle/close_room_spec.rb
@@ -3,18 +3,18 @@ require 'rails_helper'
 RSpec.describe "Battle::CloseRoom", type: :request do
   describe "POST /battle/close_room" do
     let(:room) { create(:room) }
-    let(:params) { { room_id: room.id } }
+    let(:request_params) { { room_id: room.id } }
 
     context 'with a valid request' do
       before do
-        post(battle_close_room_path, params: params)
+        post battle_close_room_path, params: request_params
       end
 
       it 'should work' do
         expect(response).to have_http_status(200)
       end
       it 'should return a empty response' do
-        expect(response_json).to be_empty
+        expect(parsed_response).to be_empty
       end
       it 'should destroy the room' do
         expect { room.reload }.to raise_error ActiveRecord::RecordNotFound
@@ -22,10 +22,12 @@ RSpec.describe "Battle::CloseRoom", type: :request do
     end
 
     context 'with a no-existing room_id' do
-      let(:params) { { room_id: -1 } }
+      let(:request_params) { { room_id: -1 } }
 
       it 'should not work' do
-        expect { post(battle_close_room_path, params: params) }.to raise_error GameError::RoomNotFound
+        expect {
+          post battle_close_room_path, params: request_params
+        }.to raise_error GameError::RoomNotFound
       end
     end
   end
