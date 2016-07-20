f31c57ff7147661f228da7e494258924ff16fe4f
Shogo Iwano <shiwano@gmail.com>
2016-07-20 22:21:44 +0900

    api: Fix join_into_room API spec

--- server/api/spec/requests/join_into_room_spec.rb
+++ server/api/spec/requests/join_into_room_spec.rb
@@ -3,26 +3,28 @@ require 'rails_helper'
 RSpec.describe 'JoinIntoRoom', type: :request do
   describe 'POST /join_into_room', with_login: true do
     let(:room) { create(:room) }
-    let(:params) { { room_id: room.id } }
+    let(:request_params) { { room_id: room.id } }
 
     context 'with a valid request' do
       before do
-        post(join_into_room_path, params: params)
+        post join_into_room_path, params: request_params
       end
 
       it 'should work' do
         expect(response).to have_http_status(200)
       end
       it 'should return the joined room' do
-        expect(response_json[:room][:id]).to eq current_user.reload.room.id
+        expect(parsed_response.room.id).to eq current_user.reload.room.id
       end
     end
 
     context 'with a no-existing room_id' do
-      let(:params) { { room_id: -1 } }
+      let(:request_params) { { room_id: -1 } }
 
       it 'should not work' do
-        expect { post(join_into_room_path, params: params) }.to raise_error GameError::RoomNotFound
+        expect {
+          post join_into_room_path, params: request_params
+        }.to raise_error GameError::RoomNotFound
       end
     end
   end
