d0b58617ea4200b185e39528fcfff2d4187dbf02
Shogo Iwano <shiwano@gmail.com>
2016-07-22 04:27:21 +0900

    client: Add Battle.ResultView

---
+++ client/Assets/Scripts/Battle/Result/ResultView.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace Submarine.Battle
+{
+    public class ResultView : MonoBehaviour
+    {
+        [SerializeField]
+        GameObject victoryEffect;
+        [SerializeField]
+        GameObject defeatEffect;
+        [SerializeField]
+        Button closeButton;
+
+        public void ShowEffect(bool isVictory)
+        {
+            gameObject.SetActive(true);
+            victoryEffect.gameObject.SetActive(isVictory);
+            defeatEffect.gameObject.SetActive(!isVictory);
+        }
+
+        public IObservable<Unit> OnCloseButtonClickAsObservable()
+        {
+            return closeButton.OnSingleClickAsObservable();
+        }
+
+        void Start()
+        {
+            victoryEffect.gameObject.SetActive(false);
+            defeatEffect.gameObject.SetActive(false);
+            gameObject.SetActive(false);
+        }
+    }
+}
