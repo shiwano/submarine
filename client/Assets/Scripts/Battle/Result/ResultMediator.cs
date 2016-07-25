22d449bbaf3149186851447e7074dfb448bdc350
Shogo Iwano <shiwano@gmail.com>
2016-07-26 00:12:49 +0900

    client: Add Battle.ResultMediator

---
+++ client/Assets/Scripts/Battle/Result/ResultMediator.cs
@@ -0,0 +1,35 @@
+﻿using UniRx;
+using Zenject;
+using Type = TyphenApi.Type.Submarine;
+
+namespace Submarine.Battle
+{
+    public class ResultMediator : IInitializable
+    {
+        [Inject]
+        UserModel userModel;
+        [Inject]
+        BattleModel battleModel;
+        [Inject]
+        ResultView view;
+        [Inject]
+        SceneChangeCommand sceneChangeCommand;
+
+        public void Initialize()
+        {
+            battleModel.OnFinishAsObservable().Take(1).Subscribe(_ => OnBattleFinish()).AddTo(view);
+            view.OnCloseButtonClickAsObservable().Take(1).Subscribe(_ => OnClose()).AddTo(view);
+        }
+
+        void OnBattleFinish()
+        {
+            var isVictory = battleModel.Winner != null && userModel.LoggedInUser.Value.Id == battleModel.Winner.Id;
+            view.ShowEffect(isVictory);
+        }
+
+        void OnClose()
+        {
+            sceneChangeCommand.Execute(SceneNames.Lobby);
+        }
+    }
+}
