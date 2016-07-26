78b082c911dfe47c4c672952cef4c63b25820bd3
Shogo Iwano <shiwano@gmail.com>
2016-07-27 07:49:40 +0900

    api: Re-generate access token when it will expires soon

--- server/api/app/controllers/application_controller.rb
+++ server/api/app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::API
   before_action :authentication_with_access_token, unless: :no_authentication_required?
+  after_action :generate_access_token_if_needed, unless: :no_authentication_required?
   rescue_from StandardError, with: :render_error unless Rails.env.test?
 
   attr_reader :current_user
@@ -17,6 +18,12 @@ class ApplicationController < ActionController::API
     raise GameError::NotAuthenticated.new('No access token') if access_token.nil?
     @current_user = User.find_by_access_token(access_token)
     raise GameError::NotAuthenticated.new('Invalid access token') if current_user.nil?
+  end
+
+  def generate_access_token_if_needed
+    if current_user.access_token.expires_soon?
+      set_access_token(current_user.generate_access_token!)
+    end
   end
 
   def render_error(e)
---
+++ server/api/spec/controllers/application_controller_spec.rb
@@ -0,0 +1,53 @@
+require 'rails_helper'
+
+RSpec.describe ApplicationController, type: :controller do
+  controller(ApplicationController) do
+    def no_authentication_required?
+      false
+    end
+
+    def service
+      render body: 'OK'
+    end
+  end
+
+  before do
+    routes.draw { post 'test' => 'anonymous#service' }
+  end
+
+  context 'with login' do
+    before do
+      @current_user = create(:user, :with_stupid_auth_token)
+      @access_token = @current_user.generate_access_token!
+      request.headers['X-Access-Token'] = @access_token
+    end
+
+    describe '#generate_access_token_if_needed' do
+      context 'when the current access token will not expires soon' do
+        it 'should generate access token' do
+          post :service
+          expect(response.headers['X-Set-Access-Token']).to be nil
+        end
+      end
+
+      context 'when the current access token will expires soon' do
+        before { Timecop.freeze(@current_user.access_token.expires_at - 10.seconds) }
+        after { Timecop.return }
+
+        it 'should generate access token' do
+          expected_access_token = 'NEW-ACCESS-TOKEN'
+          expect_any_instance_of(User).to \
+            receive(:generate_access_token!).and_return expected_access_token
+          post :service
+          expect(response.headers['X-Set-Access-Token']).to eq expected_access_token
+        end
+      end
+    end
+  end
+
+  context 'without login' do
+    it 'should raise error' do
+      expect { post :service }.to raise_error GameError::NotAuthenticated
+    end
+  end
+end
