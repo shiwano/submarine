9fecb022b2702bbd12b7d3280016e07218c88e4f
Shogo Iwano <shiwano@gmail.com>
2016-08-13 23:45:12 +0900

    client: Add commands to create bots

---
+++ client/Assets/Scripts/Battle/Commands/AddBotCommand.cs
@@ -0,0 +1,24 @@
+﻿using Zenject;
+using Zenject.Commands;
+
+namespace Submarine.Battle
+{
+    public class AddBotCommand : Command
+    {
+        public class Handler : ICommandHandler
+        {
+            [Inject]
+            BattleService battleService;
+            [Inject]
+            BattleModel battleModel;
+
+            public void Execute()
+            {
+                if (battleModel.State.Value == BattleState.InPreparation)
+                {
+                    battleService.Api.SendAddBotRequest();
+                }
+            }
+        }
+    }
+}
---
+++ client/Assets/Scripts/Battle/Commands/RemoveBotCommand.cs
@@ -0,0 +1,24 @@
+﻿using Zenject;
+using Zenject.Commands;
+
+namespace Submarine.Battle
+{
+    public class RemoveBotCommand : Command<long>
+    {
+        public class Handler : ICommandHandler<long>
+        {
+            [Inject]
+            BattleService battleService;
+            [Inject]
+            BattleModel battleModel;
+
+            public void Execute(long botId)
+            {
+                if (battleModel.State.Value == BattleState.InPreparation)
+                {
+                    battleService.Api.SendRemoveBotRequest(botId);
+                }
+            }
+        }
+    }
+}
