c0e8f9e2798d587dc0048753971f3a063b450c5c
Shogo Iwano <shiwano@gmail.com>
2016-08-27 08:02:20 +0900

    battle: Add floatEpsilon to util package

--- server/battle/src/app/util/util.go
+++ server/battle/src/app/util/util.go
@@ -4,7 +4,9 @@ import (
 	"math"
 )
 
+var floatEpsilon = 0.000001
+
 // EqualFloats determines whether the given floating point numbers are same.
 func EqualFloats(a, b float64) bool {
-	return math.Abs(a-b) < math.SmallestNonzeroFloat64
+	return math.Abs(a-b) < floatEpsilon
 }
