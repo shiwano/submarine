96d5f53aea8990babda7ee81fb99792fb15035b6
Shogo Iwano <shiwano@gmail.com>
2016-11-05 11:39:56 +0900

    battle: Define event types for actor and context

---
+++ server/battle/server/battle/context/actor_event.go
@@ -0,0 +1,41 @@
+//go:generate genem -n ActorEventEmitter -o actor_event_emitter.go $GOFILE
+
+package context
+
+import (
+	"github.com/ungerik/go3d/float64/vec2"
+
+	battleAPI "github.com/shiwano/submarine/server/battle/lib/typhenapi/type/submarine/battle"
+)
+
+type collideWithStageEvent struct {
+	point vec2.T
+}
+
+type collideWithOtherActorEvent struct {
+	actor Actor
+	point vec2.T
+}
+
+type accelerationRequestEvent struct {
+	message *battleAPI.AccelerationRequestObject
+}
+
+type brakeRequestEvent struct {
+	message *battleAPI.BrakeRequestObject
+}
+
+type turnRequestEvent struct {
+	message *battleAPI.TurnRequestObject
+}
+
+type torpedoRequestEvent struct {
+	message *battleAPI.TorpedoRequestObject
+}
+
+type pingerRequestEvent struct {
+	message *battleAPI.PingerRequestObject
+}
+
+type userLeaveEvent struct {
+}
---
+++ server/battle/server/battle/context/event.go
@@ -0,0 +1,32 @@
+//go:generate genem -n EventEmitter -o event_emitter.go $GOFILE
+
+package context
+
+import (
+	"github.com/shiwano/submarine/server/battle/lib/navmesh"
+)
+
+type actorCreateEvent struct {
+	actor Actor
+}
+
+type actorDestroyEvent struct {
+	actor Actor
+}
+
+type actorAddEvent struct {
+	actor Actor
+}
+
+type actorMoveEvent struct {
+	actor Actor
+}
+
+type actorRemoveEvent struct {
+	actor Actor
+}
+
+type actorChangeVisibilityEvent struct {
+	actor     Actor
+	teamLayer navmesh.LayerMask
+}
