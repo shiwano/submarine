570cc97ac76a5a2e02a13bf1f357e4fb6e0c2064
Shogo Iwano <shiwano@gmail.com>
2016-11-08 03:31:26 +0900

    client: Implement Radar

--- client/Assets/Scripts/Battle/RadarMediator.cs
+++ client/Assets/Scripts/Battle/RadarMediator.cs
@@ -1,14 +1,31 @@
-﻿using Zenject;
+﻿using UniRx;
+using Zenject;
 
 namespace Submarine.Battle
 {
     public class RadarMediator : IInitializable
     {
         [Inject]
+        BattleEvent.ActorCreate actorCreateEvent;
+        [Inject]
+        BattleEvent.ActorDestroy actorDestroyEvent;
+        [Inject]
         RadarView view;
 
         public void Initialize()
         {
+            actorCreateEvent.AsObservable().Subscribe(OnActorCreate).AddTo(view);
+            actorDestroyEvent.AsObservable().Subscribe(OnActorDestroy).AddTo(view);
+        }
+
+        void OnActorCreate(ActorFacade actorFacade)
+        {
+            view.CreatePin(actorFacade);
+        }
+
+        void OnActorDestroy(ActorFacade actorFacade)
+        {
+            view.DestroyPin(actorFacade);
         }
     }
 }
--- client/Assets/Scripts/Battle/RadarView.cs
+++ client/Assets/Scripts/Battle/RadarView.cs
@@ -1,4 +1,7 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
+using Type = TyphenApi.Type.Submarine;
+using System.Collections.Generic;
 
 namespace Submarine.Battle
 {
@@ -20,5 +23,68 @@ namespace Submarine.Battle
         RectTransform pinContainer;
         [SerializeField]
         Vector2 radarSize;
+
+        Dictionary<ActorFacade, RectTransform> pinsByActor = new Dictionary<ActorFacade, RectTransform>();
+        Matrix4x4 matrixForRadarPosition;
+
+        public void CreatePin(ActorFacade actorFacade)
+        {
+            GameObject pinPrefab;
+            switch (actorFacade.Actor.Type)
+            {
+                case Type.Battle.ActorType.Submarine:
+                    pinPrefab = actorFacade.IsMine ?
+                        playerPinPrefab :
+                        enemyPinPrefab;
+                    break;
+                case Type.Battle.ActorType.Torpedo:
+                    pinPrefab = torpedoPinPrefab;
+                    break;
+                default:
+                    throw new NotImplementedException("Unsupported actor type: " + actorFacade.Actor.Type);
+            }
+            var pin = Instantiate(pinPrefab).GetComponent<RectTransform>();
+            pin.SetParent(pinContainer, true);
+            pinsByActor.Add(actorFacade, pin);
+        }
+
+        public void DestroyPin(ActorFacade actorFacade)
+        {
+            RectTransform pin;
+            if (pinsByActor.TryGetValue(actorFacade, out pin))
+            {
+                Destroy(pin.gameObject);
+                pinsByActor.Remove(actorFacade);
+            }
+        }
+
+        void Awake()
+        {
+            matrixForRadarPosition = Matrix4x4.zero;
+            matrixForRadarPosition.m00 = radarSize.x / Constants.MapLength;;
+            matrixForRadarPosition.m11 = radarSize.x / Constants.MapLength;;
+        }
+
+        void Update()
+        {
+            foreach (var pair in pinsByActor)
+            {
+                pair.Value.gameObject.SetActive(pair.Key.Actor.IsVisible);
+                pair.Value.localPosition = GetRadarPosition(pair.Key);
+                pair.Value.localRotation = GetRadarRotation(pair.Key);
+            }
+        }
+
+        Vector3 GetRadarPosition(ActorFacade actorFacade)
+        {
+            var position = actorFacade.View.ActorPosition;
+            return matrixForRadarPosition.MultiplyPoint3x4(new Vector3(position.x, position.y));
+        }
+
+        Quaternion GetRadarRotation(ActorFacade actorFacade)
+        {
+            var eulerAngles = new Vector3(0f, 0f, actorFacade.View.ActorDirection - 90f);
+            return Quaternion.Euler(eulerAngles);
+        }
     }
 }
