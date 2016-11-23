7b4d0d0663767cbd1875c2230bbd5b1ce9fcf38b
Shogo Iwano <shiwano@gmail.com>
2016-11-23 13:48:47 +0900

    battle: Return debugger window as navmesh.Debugger interface

--- server/battle/main_debug.go
+++ server/battle/main_debug.go
@@ -5,13 +5,14 @@ package main
 import (
 	"runtime"
 
+	"github.com/shiwano/submarine/server/battle/lib/navmesh"
 	"github.com/shiwano/submarine/server/battle/lib/navmesh/debugger"
 	"github.com/shiwano/submarine/server/battle/server"
 	"github.com/shiwano/submarine/server/battle/server/debug"
 )
 
 func main() {
-	debugger.Main(func(d *debugger.Debugger) {
+	debugger.Main(func(d navmesh.Debugger) {
 		debug.Debugger = d
 		s := server.New()
 		s.Run(":5000")
