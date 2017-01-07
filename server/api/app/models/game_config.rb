8ed5a059b24f713a4a564a4efd6862afdad47781
Shogo Iwano <shiwano@gmail.com>
2017-01-08 02:09:23 +0900

    api: Change config file path

--- server/api/app/models/game_config.rb
+++ server/api/app/models/game_config.rb
@@ -1,7 +1,7 @@
 class GameConfig
-  open("#{Rails.root}/../../config.#{Rails.env.test? ? 'example' : Rails.env}.yml") do |file|
+  open("#{Rails.root}/config#{Rails.env.test? ? '.example' : ''}.yml") do |file|
     hash = YAML.load(file)
-    config = Hashie::Mash.new(hash).server
+    config = Hashie::Mash.new(hash)
 
     config.keys.each do |key|
       define_singleton_method(key) do |*args|
