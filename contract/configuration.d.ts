cc74e582a0e894b02acf21b5bc75e099eb38d583
Shogo Iwano <shiwano@gmail.com>
2017-01-08 23:44:33 +0900

    contract: Add Configuration.Server

--- contract/configuration.d.ts
+++ contract/configuration.d.ts
@@ -3,4 +3,15 @@ declare module Submarine.Configuration {
     version: string;
     apiServerBaseUri: string;
   }
+
+  interface Server {
+    apiServerBaseUri: string;
+    battleServerBaseUri: string;
+    database: {
+      host: string;
+      port: integer;
+      user: string;
+      password: string;
+    }
+  }
 }
