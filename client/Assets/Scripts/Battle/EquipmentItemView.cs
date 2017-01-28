7e5483811f63001a1d94f8751e9c75e24c15b551
Shogo Iwano <shiwano@gmail.com>
2017-01-29 01:31:33 +0900

    client: Add EquipmentView and EquipmentItemButtonView

---
+++ client/Assets/Scripts/Battle/EquipmentItemView.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace Submarine.Battle
+{
+    [RequireComponent(typeof(Button))]
+    public class EquipmentItemView : MonoBehaviour
+    {
+        [SerializeField]
+        Text cooldownText;
+
+        Button button;
+
+        public IObservable<Unit> OnUseAsObservable()
+        {
+            return button.OnSingleClickAsObservable();
+        }
+
+        void Awake()
+        {
+            button = GetComponent<Button>();
+        }
+    }
+}
---
+++ client/Assets/Scripts/Battle/EquipmentView.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UniRx;
+
+namespace Submarine.Battle
+{
+    public class EquipmentView : MonoBehaviour, BattleInputService.IEquipmentInput
+    {
+        [SerializeField]
+        EquipmentItemView decoy;
+        [SerializeField]
+        EquipmentItemView pinger;
+        [SerializeField]
+        EquipmentItemView watcher;
+
+        [SerializeField]
+        Image torpedoResourceImageTemplate;
+        [SerializeField]
+        RectTransform torpedoResourceImageRoot;
+        [SerializeField]
+        Vector3 torpedoResourceImageOffset;
+
+        public IObservable<Unit> OnDecoyUseAsObservable()   { return decoy.OnUseAsObservable(); }
+        public IObservable<Unit> OnPingerUseAsObservable()  { return pinger.OnUseAsObservable(); }
+        public IObservable<Unit> OnWatcherUseAsObservable() { return watcher.OnUseAsObservable(); }
+    }
+}
