12d5bae43127c1541fccbbb7feb7b0493b3533cf
Shogo Iwano <shiwano@gmail.com>
2017-01-29 02:19:49 +0900

    client: Extend EquipmentItem class

---
+++ client/Assets/Scripts/TyphenApi/Type/Submarine/Battle/EquipmentItem.partial.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Submarine;
+
+namespace TyphenApi.Type.Submarine.Battle
+{
+    public partial class EquipmentItem
+    {
+        public DateTime CooldownStartedAtAsDateTime
+        {
+            get { return CurrentMillis.FromMilliseconds(CooldownStartedAt); }
+        }
+
+        public DateTime CooldownFinishedAtAsDateTime
+        {
+            get { return CurrentMillis.FromMilliseconds(CooldownStartedAt + CooldownDuration); }
+        }
+    }
+}
