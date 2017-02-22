67dc2c8faf4db772ba8abdbbaa658b0daa41b141
Shogo Iwano <shiwano@gmail.com>
2017-02-23 00:35:03 +0900

    client: Add ZenjectExtensions

---
+++ client/Assets/Scripts/Utility/ZenjectExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Zenject;
+
+namespace Submarine
+{
+    public static class ZenjectExtensions
+    {
+        public static void BindMediatorAndViewAsSingle<TMediator, TView>(this DiContainer container, TView view)
+            where TMediator : MediatorBase<TView>
+            where TView : IView
+        {
+            container.BindInterfacesAndSelfTo<TMediator>().AsSingle();
+            container.Bind<TView>().FromInstance(view).AsSingle();
+        }
+    }
+}
