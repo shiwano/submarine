91dbfa9abdba32cbd2b8a8ec3f0b5ca229045bc3
Shogo Iwano <shiwano@gmail.com>
2017-03-13 23:44:04 +0900

    client: Install MessagePack-CSharp v1.0.1

---
+++ client/Assets/Vendor/MessagePack/LZ4/Codec/LZ4Codec.Helper.cs
@@ -0,0 +1,70 @@
+﻿#if ENABLE_UNSAFE_MSGPACK
+
+using System;
+
+namespace MessagePack.LZ4
+{
+    public static partial class LZ4Codec
+    {
+        public static int Encode(byte[] input, int inputOffset, int inputLength, byte[] output, int outputOffset, int outputLength)
+        {
+            if (IntPtr.Size == 4)
+            {
+                return LZ4Codec.Encode32Unsafe(input, inputOffset, inputLength, output, outputOffset, outputLength);
+            }
+            else
+            {
+                return LZ4Codec.Encode64Unsafe(input, inputOffset, inputLength, output, outputOffset, outputLength);
+            }
+        }
+
+        public static int Decode(byte[] input, int inputOffset, int inputLength, byte[] output, int outputOffset, int outputLength)
+        {
+            if (IntPtr.Size == 4)
+            {
+                return LZ4Codec.Decode32Unsafe(input, inputOffset, inputLength, output, outputOffset, outputLength);
+            }
+            else
+            {
+                return LZ4Codec.Decode64Unsafe(input, inputOffset, inputLength, output, outputOffset, outputLength);
+            }
+        }
+
+        internal unsafe static class HashTablePool
+        {
+            [ThreadStatic]
+            static ushort[] ushortPool;
+
+            [ThreadStatic]
+            static uint[] uintPool;
+
+            public static ushort[] GetUShortHashTablePool()
+            {
+                if (ushortPool == null)
+                {
+                    ushortPool = new ushort[HASH64K_TABLESIZE];
+                }
+                else
+                {
+                    Array.Clear(ushortPool, 0, ushortPool.Length);
+                }
+                return ushortPool;
+            }
+
+            public static uint[] GetUIntHashTablePool()
+            {
+                if (uintPool == null)
+                {
+                    uintPool = new uint[HASH_TABLESIZE];
+                }
+                else
+                {
+                    Array.Clear(uintPool, 0, uintPool.Length);
+                }
+                return uintPool;
+            }
+        }
+    }
+}
+
+#endif
---
+++ client/Assets/Vendor/MessagePack/Resolvers/StandardResolver.cs
@@ -0,0 +1,95 @@
+﻿using MessagePack.Formatters;
+
+namespace MessagePack.Resolvers
+{
+    /// <summary>
+    /// Default composited resolver, builtin -> dynamic enum -> dynamic generic -> dynamic union -> dynamic object.
+    /// </summary>
+    public class StandardResolver : IFormatterResolver
+    {
+        public static IFormatterResolver Instance = new StandardResolver();
+
+        static readonly IFormatterResolver[] resolvers = new[]
+        {
+            BuiltinResolver.Instance, // Try Builtin
+
+#if !NETSTANDARD1_4
+            MessagePack.Unity.UnityResolver.Instance,
+#endif
+
+#if !ENABLE_IL2CPP
+
+            DynamicEnumResolver.Instance, // Try Enum
+            DynamicGenericResolver.Instance, // Try Array, Tuple, Collection
+            DynamicUnionResolver.Instance, // Try Union(Interface)
+            DynamicObjectResolver.Instance // Try Object
+
+#endif
+        };
+
+        StandardResolver()
+        {
+        }
+
+        public IMessagePackFormatter<T> GetFormatter<T>()
+        {
+            return FormatterCache<T>.formatter;
+        }
+
+        static class FormatterCache<T>
+        {
+            public static readonly IMessagePackFormatter<T> formatter;
+
+            static FormatterCache()
+            {
+                foreach (var item in resolvers)
+                {
+                    var f = item.GetFormatter<T>();
+                    if (f != null)
+                    {
+                        formatter = f;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    public class ContractlessStandardResolver : IFormatterResolver
+    {
+        public static IFormatterResolver Instance = new ContractlessStandardResolver();
+
+        static readonly IFormatterResolver[] resolvers = new[]
+        {
+            StandardResolver.Instance,
+            DynamicContractlessObjectResolver.Instance,
+        };
+
+        ContractlessStandardResolver()
+        {
+        }
+
+        public IMessagePackFormatter<T> GetFormatter<T>()
+        {
+            return FormatterCache<T>.formatter;
+        }
+
+        static class FormatterCache<T>
+        {
+            public static readonly IMessagePackFormatter<T> formatter;
+
+            static FormatterCache()
+            {
+                foreach (var item in resolvers)
+                {
+                    var f = item.GetFormatter<T>();
+                    if (f != null)
+                    {
+                        formatter = f;
+                        return;
+                    }
+                }
+            }
+        }
+    }
+}
