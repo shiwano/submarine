b1e3d7ce5fb07353a9f2c03a3bd47224ca9f0e20
Shogo Iwano <shiwano@gmail.com>
2017-03-15 23:51:11 +0900

    tools: Remove codec package from go templates

---
+++ tools/typhen/typhen-api/lib/go/templates/core/json_serializer.go
@@ -0,0 +1,41 @@
+package typhenapi
+
+import (
+	"bytes"
+	"encoding/json"
+	"errors"
+)
+
+// JSONSerializer is Serializer/Deserializer for TyphenAPI types.
+type JSONSerializer struct{}
+
+// Serialize a TyphenAPI type to bytes.
+func (s *JSONSerializer) Serialize(v interface{}) (data []byte, err error) {
+	b := &bytes.Buffer{}
+
+	encoder := json.NewEncoder(b)
+	if err := encoder.Encode(v); err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
+}
+
+// Deserialize bytes to a TyphenAPI type.
+func (s *JSONSerializer) Deserialize(b []byte, v interface{}) error {
+	r := bytes.NewReader(b)
+	decoder := json.NewDecoder(r)
+
+	if err := decoder.Decode(v); err != nil {
+		return err
+	}
+
+	result := v.(Type)
+	if result == nil {
+		return errors.New("Not TyphenAPI type")
+	}
+
+	if err := result.Coerce(); err != nil {
+		return err
+	}
+	return nil
+}
